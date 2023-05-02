3b5d47cf0dca877aca113d08abdad1e0028c1a37
Zhenwu Dai <82241407+realzwu@users.noreply.github.com>
2023-05-02 21:58:12 +0800

    Add files via upload

--- tcia_dicom_to_nifti.py
+++ tcia_dicom_to_nifti.py
@@ -14,33 +14,31 @@ def get_all_dicom_files(root_folder):
 
 
 def convert_dicom_folder_to_nifti(input_folder, output_folder):
-    # 遍历输入文件夹中的子文件夹
+    # list all dicom files in the folders (sub-folders)
     for subdir in sorted(os.listdir(input_folder)):
         subdir_path = os.path.join(input_folder, subdir)
 
-        # 检查是否为文件夹
         if os.path.isdir(subdir_path):
-            # 创建输出文件夹
+            # create output folder, and get series of dicom files
             os.makedirs(output_folder, exist_ok=True)
-
-            # 获取所有DICOM文件
             dicom_files = get_all_dicom_files(subdir_path)
 
             if not dicom_files:
                 print(f"No DICOM files found in {subdir_path}. Skipping...")
                 continue
 
-            # 读取DICOM序列并转换为NIfTI格式
+            # hundreds of dicom files to single nifti file
             reader = sitk.ImageSeriesReader()
             reader.SetFileNames(dicom_files)
             image = reader.Execute()
 
-            # 保存NIfTI文件
+            # save_nifti_files
             nifti_file = os.path.join(output_folder, f"{subdir}.nii.gz")
             sitk.WriteImage(image, nifti_file)
 
 
-'''# SPIE-AAPM Lung CT Challenge
+'''
+# SPIE-AAPM Lung CT Challenge
 input_folder = r"F:\sarcopenia-unet\data\TCIA\dicom\manifest-1682752878304\SPIE-AAPM Lung CT Challenge"
 output_folder = r"F:\sarcopenia-unet\data\TCIA\nifti\SPIE-AAPM Lung CT Challenge"
 
@@ -48,8 +46,8 @@ convert_dicom_folder_to_nifti(input_folder, output_folder)
 
 # LungCT-Diagnosis: unclear
 # Qin Lung CT: not cover T12
-
 '''
+
 # Lung CT Segmentation Challenge 2017 (LCTSC)
 input_folder = r"F:\sarcopenia-unet\data\TCIA\dicom\manifest-1683006587994\LCTSC"
 output_folder = r"F:\sarcopenia-unet\data\TCIA\nifti\LCTSC"
