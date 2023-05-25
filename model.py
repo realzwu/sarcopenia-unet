368a031a1e00e2cdaa2d319aa9bc12b242c23531
Zhenwu Dai <82241407+realzwu@users.noreply.github.com>
2023-05-25 22:29:28 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -1,185 +1,78 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
 
-class ConvBlock(nn.Module):
-
+class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(ConvBlock, self).__init__()
-
-        # number of input channels is a number of filters in the previous layer
-        # number of output channels is a number of filters in the current layer
-        # "same" convolutions
+        super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
-        x = self.conv(x)
-        return x
+        return self.conv(x)
 
+class UNET(nn.Module):
+    def __init__(
+            self, in_channels=1, out_channels=1, features=[64, 128, 256, 512],
+    ):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-class UpConv(nn.Module):
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
 
-    def __init__(self, in_channels, out_channels):
-        super(UpConv, self).__init__()
+        # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature*2, feature, kernel_size=2, stride=2,
+                )
+            )
+            self.ups.append(DoubleConv(feature*2, feature))
 
-        self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        x = self.up(x)
-        return x
-
-
-class AttentionBlock(nn.Module):
-    """Attention block with learnable parameters"""
-
-    def __init__(self, F_g, F_l, n_coefficients):
-        """
-        :param F_g: number of feature maps (channels) in previous layer
-        :param F_l: number of feature maps in corresponding encoder layer, transferred via skip connection
-        :param n_coefficients: number of learnable multi-dimensional attention coefficients
-        """
-        super(AttentionBlock, self).__init__()
-
-        self.W_gate = nn.Sequential(
-            nn.Conv2d(F_g, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(n_coefficients)
-        )
-
-        self.W_x = nn.Sequential(
-            nn.Conv2d(F_l, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(n_coefficients)
-        )
-
-        self.psi = nn.Sequential(
-            nn.Conv2d(n_coefficients, 1, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
-        )
-
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, gate, skip_connection):
-        """
-        :param gate: gating signal from previous layer
-        :param skip_connection: activation from corresponding encoder layer
-        :return: output activations
-        """
-        g1 = self.W_gate(gate)
-        x1 = self.W_x(skip_connection)
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-        out = skip_connection * psi
-        return out
-
-
-class AttentionUNet(nn.Module):
-
-    def __init__(self, img_ch=1, output_ch=1):
-        super(AttentionUNet, self).__init__()
-
-        self.MaxPool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.Conv1 = ConvBlock(img_ch, 64)
-        self.Conv2 = ConvBlock(64, 128)
-        self.Conv3 = ConvBlock(128, 256)
-        self.Conv4 = ConvBlock(256, 512)
-        self.Conv5 = ConvBlock(512, 1024)
-
-        self.Up5 = UpConv(1024, 512)
-        self.Att5 = AttentionBlock(F_g=512, F_l=512, n_coefficients=256)
-        self.UpConv5 = ConvBlock(1024, 512)
-
-        self.Up4 = UpConv(512, 256)
-        self.Att4 = AttentionBlock(F_g=256, F_l=256, n_coefficients=128)
-        self.UpConv4 = ConvBlock(512, 256)
-
-        self.Up3 = UpConv(256, 128)
-        self.Att3 = AttentionBlock(F_g=128, F_l=128, n_coefficients=64)
-        self.UpConv3 = ConvBlock(256, 128)
-
-        self.Up2 = UpConv(128, 64)
-        self.Att2 = AttentionBlock(F_g=64, F_l=64, n_coefficients=32)
-        self.UpConv2 = ConvBlock(128, 64)
-
-        self.Conv = nn.Conv2d(64, output_ch, kernel_size=1, stride=1, padding=0)
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def forward(self, x):
-        """
-        e : encoder layers
-        d : decoder layers
-        s : skip-connections from encoder layers to decoder layers
-        """
-        e1 = self.Conv1(x)
+        skip_connections = []
 
-        e2 = self.MaxPool(e1)
-        e2 = self.Conv2(e2)
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
 
-        e3 = self.MaxPool(e2)
-        e3 = self.Conv3(e3)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
 
-        e4 = self.MaxPool(e3)
-        e4 = self.Conv4(e4)
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
 
-        e5 = self.MaxPool(e4)
-        e5 = self.Conv5(e5)
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
 
-        d5 = self.Up5(e5)
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
 
-        s4 = self.Att5(gate=d5, skip_connection=e4)
-        d5 = torch.cat((s4, d5), dim=1) # concatenate attention-weighted skip connection with previous layer output
-        d5 = self.UpConv5(d5)
-
-        d4 = self.Up4(d5)
-        s3 = self.Att4(gate=d4, skip_connection=e3)
-        d4 = torch.cat((s3, d4), dim=1)
-        d4 = self.UpConv4(d4)
-
-        d3 = self.Up3(d4)
-        s2 = self.Att3(gate=d3, skip_connection=e2)
-        d3 = torch.cat((s2, d3), dim=1)
-        d3 = self.UpConv3(d3)
-
-        d2 = self.Up2(d3)
-        s1 = self.Att2(gate=d2, skip_connection=e1)
-        d2 = torch.cat((s1, d2), dim=1)
-        d2 = self.UpConv2(d2)
-
-        out = self.Conv(d2)
-
-        return out
-
+        return self.final_conv(x)
 
 def test():
-    # Set the device to "cuda" if available, else "cpu"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Create a random input tensor with shape (3, 1, 161, 161)
-    x = torch.randn((3, 1, 512, 512)).to(device)
-
-    # Instantiate the UNET model with input channels 1 and output channels 1
-    model = AttentionUNet(img_ch=1, output_ch=1).to(device)
-
-    # Generate predictions using the model
+    x = torch.randn((3, 1, 161, 161))
+    model = UNET(in_channels=1, out_channels=1)
     preds = model(x)
-
-    # Print the shape of the predictions and the input tensor
-    print("Predictions shape:", preds.shape)
-    print("Input tensor shape:", x.shape)
-
-    # Check if the predictions have the same shape as the input tensor
+    print(preds.shape)
+    print(x.shape)
     assert preds.shape == x.shape
-
 
 if __name__ == "__main__":
     test()
