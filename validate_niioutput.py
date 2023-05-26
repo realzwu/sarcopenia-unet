6c7504e264fd9d28f79beaf1c81f401d3a67f145
Mitch Chen <30834970+scat2801@users.noreply.github.com>
2023-05-27 00:20:44 +0100

    Add files via upload
    added output to nifti functionality

--- validate_niioutput.py
+++ validate_niioutput.py
@@ -9,6 +9,7 @@ from model import UNET
 from utils import load_checkpoint
 import albumentations as A
 import nibabel as nib
+from matplotlib import pyplot as plt
 
 def running_slices(ct_dir, slice_dir, output_ct):
     # we have a series of CT volumes and T4 segmentations
@@ -45,23 +46,6 @@ def running_slices(ct_dir, slice_dir, output_ct):
 
         np.save(os.path.join(output_ct, f"{caseid}.npy"), ct_2d)
 
-        case_mask = "F:\\sarcopenia-unet\\data\\nifti\\test_t12\\77941.nii.gz"
-        img_nii = nib.load(case_mask)
-        mask = img_nii.get_fdata()
-
-        for x in range(mask.shape[0]):
-            for y in range(mask.shape[1]):
-                for z in range (mask.shape[2]):
-                    if (mask[x,y,z] != 0):
-                        T12_slice = z
-                        break
-
-        mask_output = np.zeros(mask.shape)
-        mask_output[:, :, T12_slice] = np.int16(ct_2d)
-
-        output_image = nib.Nifti1Image(mask_output, affine=img_nii.affine, dtype='mask' )
-        nib.save(output_image,  "F:\\sarcopenia-unet\\data\\nifti\\test_t12\\output_mask.nii.gz")
-
         ct_2d = np.clip(ct_2d, -100, None)
 
         transform = A.Normalize(mean=[0.0], std=[1.0])
@@ -71,17 +55,63 @@ def running_slices(ct_dir, slice_dir, output_ct):
 
         model = UNET(in_channels=1, out_channels=1).to("cuda")
 
-        load_checkpoint(torch.load(os.path.join(ROOT_DIR, "t12_checkpoint.pth.tar")), model)
+        load_checkpoint(torch.load(os.path.join(ROOT_DIR, f"model\\t12_checkpoint.pth.tar")), model)
 
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
 
-        torchvision.utils.save_image(preds, os.path.join(ROOT_DIR, f"output/validate_images/pred_{caseid}.png"))
+        torchvision.utils.save_image(preds, os.path.join(ROOT_DIR, f"data\\png\\pred_{caseid}.png"))
+
+        np_arr = preds.detach().cpu().numpy()
+
+        case_mask = os.path.join(ROOT_DIR, f"data\\mask\\92.nii.gz")
+        mask_nii = nib.load(case_mask)
+        mask = mask_nii.get_fdata()
+
+        for x in range(mask.shape[0]):
+            for y in range(mask.shape[1]):
+                for z in range (mask.shape[2]):
+                    if (mask[x,y,z] != 0):
+                        T12_slice = z
+                        break
+
+        mask_output = mask
+        mask_output[:, :, T12_slice] = np.rot90(np_arr, 3, (2,3))
+
+        #plt.imshow(mask_output[:, :, T12_slice], interpolation='nearest')
+        #plt.show()
+
+        '''
+        cos_gamma = np.cos(4.71239)
+        sin_gamma = np.sin(4.71239)
+        #rotation_affine = np.array([[1, 0, 0, 0],
+        #                            [0, cos_gamma, -sin_gamma, 0],
+        #                            [0, sin_gamma, cos_gamma, 0],
+        #                            [0, 0, 0, 1]])
+
+        rotation_affine = np.array([[cos_gamma, -sin_gamma, 0, 0],
+                                    [sin_gamma, cos_gamma, 0, 0],
+                                    [0, 0, 1, 0],
+                                    [0, 0, 0, 1]])
+        affine_rot = rotation_affine.dot(mask_nii.affine)
+        '''
+
+        output_image = nib.Nifti1Image(mask_output, affine=mask_nii.affine, header=mask_nii.header, dtype='mask')
+
+        ornt = np.array([[0, 1],
+                         [1, -1],
+                         [2, 1]])
+
+        output_image = output_image.as_reoriented(ornt)
+
+        nib.save(output_image, os.path.join(ROOT_DIR, f"data\\output\\92_output.nii.gz"))
+
+
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-ct_dir = os.path.join(ROOT_DIR, 'data/nifti/test_ct')
+ct_dir = os.path.join(ROOT_DIR, 'data/image/')
 
 '''
 def testing_t4():
@@ -92,8 +122,8 @@ def testing_t4():
 '''
 
 def testing_t12():
-    t12_dir = os.path.join(ROOT_DIR, 'data/nifti/test_t12')
-    output_ct_t12 = os.path.join(ROOT_DIR, 'data/nifti/npy_t12')
+    t12_dir = os.path.join(ROOT_DIR, 'data/mask/')
+    output_ct_t12 = os.path.join(ROOT_DIR, 'data/npy')
     sys.stdout = Logger(os.path.join(ROOT_DIR, 't12.txt'))
     running_slices(ct_dir, t12_dir, output_ct_t12)
 
